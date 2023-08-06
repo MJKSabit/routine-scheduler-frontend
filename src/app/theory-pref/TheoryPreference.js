75e741940f1e4113158c39a5a607953356e70444
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-08-07 04:04:38 +0600

    Theory Pref

--- src/app/theory-pref/TheoryPreference.js
+++ src/app/theory-pref/TheoryPreference.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { getStatus, initiate } from "../api/theory-assign";
+import { Alert } from "react-bootstrap";
 
 export default function TheoryPreference() {
   const [status, setStatus] = useState({ status: 0, values: [] });
@@ -42,12 +43,12 @@ export default function TheoryPreference() {
                   disabled={status.status !== 0}
                   type="button"
                   className="btn btn-rounded btn-light btn-sm float-right position-relative z-index-3"
-                  onClick={e => {
-                    initiate().then(res => {
+                  onClick={(e) => {
+                    initiate().then((res) => {
                       getStatus().then((res) => {
                         setStatus({ values: [], ...res });
                       });
-                    })
+                    });
                   }}
                 >
                   <i
@@ -70,35 +71,43 @@ export default function TheoryPreference() {
           <div className="card">
             <div className="card-body">
               <h4 className="card-title">Yet to submit the form </h4>
-              <div className="table-responsive">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th> Initial </th>
-                      <th> Name </th>
-                      <th> Email </th>
-                      <th> Action </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {status.values.map((teacher, index) => (
-                      <tr key={index}>
-                        <td> {teacher.initial} </td>
-                        <td> {teacher.name} </td>
-                        <td> {teacher.email} </td>
-                        <td>
-                          <button
-                            type="button"
-                            className="btn btn-primary btn-sm"
-                          >
-                            Resend
-                          </button>
-                        </td>
+              {status.values.length !== 0 && (
+                <div className="table-responsive">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th> Initial </th>
+                        <th> Name </th>
+                        <th> Email </th>
+                        <th> Action </th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                    </thead>
+                    <tbody>
+                      {status.values.map((teacher, index) => (
+                        <tr key={index}>
+                          <td> {teacher.initial} </td>
+                          <td> {teacher.name} </td>
+                          <td> {teacher.email} </td>
+                          <td>
+                            <button
+                              type="button"
+                              className="btn btn-primary btn-sm"
+                            >
+                              Resend
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+              {status.values.length === 0 && status.status >= 2 && (
+                <Alert variant="success text-center">All submitted, waiting for next phase</Alert>
+              )}
+              {status.values.length === 0 && status.status == 0 && (
+                <Alert variant="warning text-center">Initialize the process to send email</Alert>
+              )}
             </div>
           </div>
         </div>
@@ -107,9 +116,9 @@ export default function TheoryPreference() {
         <div className="col stretch-card grid-margin">
           <div
             className={`card bg-gradient-${
-              status.status < 3
+              status.status < 2
                 ? "danger"
-                : status.status === 3
+                : status.status === 2
                 ? "info"
                 : "success"
             } card-img-holder text-white`}
@@ -125,13 +134,17 @@ export default function TheoryPreference() {
               <h4 className="font-weight-normal mb-3">
                 Assign Teachers according to Seniorty{" "}
                 <button
-                  disabled={status.status !== 3}
+                  disabled={status.status !== 2}
                   type="button"
                   className="btn btn-rounded btn-light btn-sm float-right position-relative z-index-3"
                 >
                   <i
                     className={`mdi ${
-                      status.status < 3 ? "mdi-cancel" : status.status === 3 ? "mdi-autorenew" : "mdi-check"
+                      status.status < 2
+                        ? "mdi-cancel"
+                        : status.status === 2
+                        ? "mdi-autorenew"
+                        : "mdi-check"
                     } mdi-24px float-right`}
                   ></i>
                 </button>
