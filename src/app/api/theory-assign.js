65e3001b8dda8d589f84a50d98045d46ca1cc630
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-08-07 02:42:19 +0600

    Admin Theory Assign Page

---
+++ src/app/api/theory-assign.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { api_url } from ".";
+
+export const getStatus = () =>
+  axios.get(api_url("/assign/theory/status")).then((res) => res.data);
+
+export const initiate = () =>
+    axios.post(api_url("/assign/theory/initiate")).then((res) => res.data);
