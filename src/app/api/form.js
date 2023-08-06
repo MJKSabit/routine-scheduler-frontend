718fda612dbadf9fe3d6431b5387a67f1538a62b
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-08-07 03:48:23 +0600

    Form Submission

--- src/app/api/form.js
+++ src/app/api/form.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { api_url } from ".";
 
-const getOfferedCourses = () =>
-    axios.get(api_url("/course")).then((res) => res.data);
+export const getTheoryPreferencesForm = form_id =>
+    axios.get(api_url(`/forms/${form_id}`)).then(res => res.data.data);
+
+export const submitTheoryPreferencesForm = (form_id, data) =>
+    axios.put(api_url(`/forms/${form_id}`), data).then(res => res.data);
