04fa21a9b14dc8a603471c7c718028f43cb601c1
alfahadarc <alfahadarc@gmail.com>
2023-09-11 01:31:47 +0600

    front can show pdf

--- src/app/api/pdf.js
+++ src/app/api/pdf.js
@@ -3,4 +3,17 @@ import { api_url } from ".";
 
 
 export const getPdfForStudent = (lvlTerm, section) =>
-  axios.get(api_url(`/pdf/showTerm/${lvlTerm}/${section}`)).then((res) => res.data);
+    axios.get(api_url(`/pdf/showTerm/${lvlTerm}/${section}`), { responseType: 'blob' }).then((res) => res.data);
+
+export const getAllInitial = () =>
+    axios.get(api_url(`/pdf/allInitial`)).then((res) => res.data);
+
+
+export const getPdfForTeacher = (initial) =>
+    axios.get(api_url(`/pdf/showTeacher/${initial}`), { responseType: 'blob' }).then((res) => res.data);
+
+export const getPdfForRoom = (initial) =>
+    axios.get(api_url(`/pdf/showRoom/${initial}`), { responseType: 'blob' }).then((res) => res.data);
+
+export const getAllRooms = () =>
+    axios.get(api_url(`/pdf/allRooms`)).then((res) => res.data);
--- src/app/pdf/ShowPdf.js
+++ src/app/pdf/ShowPdf.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Dropdown, DropdownButton, Form, FormGroup, Button } from "react-bootstrap";
-import { getPdfForStudent } from '../api/pdf';
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Form, FormGroup, Button } from "react-bootstrap";
+import { getAllInitial, getPdfForStudent, getPdfForTeacher , getAllRooms,getPdfForRoom } from '../api/pdf';
 
 export default function ShowPdf() {
 
@@ -8,12 +8,42 @@ export default function ShowPdf() {
     const [forTeacher, setForTeacher] = useState(false);
     const [forRoom, setForRoom] = useState(false);
 
+
+
+    const [initials, setInitials] = useState([]);
+
+    useEffect(() => {
+        getAllInitial().then((res) => {
+            setInitials(res.initials);
+        });
+    }, []);
+
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        getAllRooms().then((res) => {
+            setRooms(res.rooms);
+        });
+    }, []);
+
+    const [selectedInitial, setSelectedInitial] = useState('');
+    const handleSelectInitial = (selectedOption) => {
+        setSelectedInitial(selectedOption);
+    };
+    const [selectedRoom, setSelectedRoom] = useState('');
+    const handleSelectRoom = (selectedOption) => {
+        setSelectedRoom(selectedOption);
+    }
+
     const [lvlTerm, setLvlTerm] = useState('L4-T1');
     const [section, setSection] = useState('A');
+    
 
     const [pdfData, setpdfData] = useState('');
 
-    const handleSelect = (selectedOption) => {
+    const handleSelect = (e) => {
+        const selectedOption = e.target.value;
+        console.log(selectedOption);
         setForStudent(false);
         setForTeacher(false);
         setForRoom(false);
@@ -41,11 +71,24 @@ export default function ShowPdf() {
         setSection(event.target.value);
     };
 
+   
+
     const displayPdf = () => {
         try {
             if (forStudent) {
                 getPdfForStudent(lvlTerm, section).then((res) => {
-                    setpdfData(res);
+                    const pdfBlob = new Blob([res], { type: 'application/pdf' });
+                    setpdfData(pdfBlob);
+                });
+            } else if (forTeacher) {
+                getPdfForTeacher(selectedInitial).then((res) => {
+                    const pdfBlob = new Blob([res], { type: 'application/pdf' });
+                    setpdfData(pdfBlob);
+                });
+            } else if (forRoom) {
+                getPdfForRoom(selectedRoom).then((res) => {
+                    const pdfBlob = new Blob([res], { type: 'application/pdf' });
+                    setpdfData(pdfBlob);
                 });
             }
         } catch (err) {
@@ -58,123 +101,164 @@ export default function ShowPdf() {
 
     return (
 
-        <Container>
-            <Row>
+        <Container className="mt-4">
+            <Row className="mb-3">
                 <Col>
-                    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                        <Dropdown.Item onClick={() => handleSelect('For Student')}>For Student</Dropdown.Item>
-                        <Dropdown.Item onClick={() => handleSelect('For Teacher')}>For Teacher</Dropdown.Item>
-                        <Dropdown.Item onClick={() => handleSelect('For Room')}>For Room</Dropdown.Item>
-                    </DropdownButton>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>select type</Form.Label>
+                        <Form.Control
+                            as="select"
+                            onChange={handleSelect}
+                            style={{ width: "25%" }}
+                        >
+                            <option value={'For Student'}>For Student</option>
+                            <option value={'For Teacher'}>For Teacher</option>
+                            <option value={'For Room'}>For Room</option>
+                        </Form.Control>
+                    </Form.Group>
                 </Col>
             </Row>
-            <Row>
 
-                {forStudent &&
-                    <>
-                        <Row>
-                            <Col>
-                                <div>
-                                    <Form>
-                                        <FormGroup>
-                                            <div key="inline-radio" style={{ display: 'flex', alignItems: 'center' }}>
-                                                <Form.Check
-                                                    inline
-                                                    label="L4-T1"
-                                                    name="group1"
-                                                    type="radio"
-                                                    id="inline-radio-1"
-                                                    value="L4-T1"
-                                                    checked={lvlTerm === 'L4-T1'}
-                                                    onChange={handleRadioChange}
-                                                />
-                                                <Form.Check
-                                                    inline
-                                                    label="L3-T2"
-                                                    name="group1"
-                                                    type="radio"
-                                                    id="inline-radio-1"
-                                                    value="L3-T2"
-                                                    checked={lvlTerm === 'L3-T2'}
-                                                    onChange={handleRadioChange}
-                                                />
-                                                <Form.Check
-                                                    inline
-                                                    label="L2-T2"
-                                                    name="group1"
-                                                    type="radio"
-                                                    id="inline-radio-1"
-                                                    value="L2-T2"
-                                                    checked={lvlTerm === 'L2-T2'}
-                                                    onChange={handleRadioChange}
-                                                />
-                                                <Form.Check
-                                                    inline
-                                                    label="L1-T2"
-                                                    name="group1"
-                                                    type="radio"
-                                                    id="inline-radio-1"
-                                                    value="L1-T2"
-                                                    checked={lvlTerm === 'L1-T2'}
-                                                    onChange={handleRadioChange}
-                                                />
-                                            </div>
-                                        </FormGroup>
-                                    </Form>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form>
-                                    <FormGroup>
-                                        <div key="inline-radio" style={{ display: 'flex', alignItems: 'center' }}>
-                                            <Form.Check
-                                                inline
-                                                label="A"
-                                                name="group1"
-                                                type="radio"
-                                                id="inline-radio-1"
-                                                value="A"
-                                                checked={section === 'A'}
-                                                onChange={handleRadioChangeSection}
-                                            />
-                                            <Form.Check
-                                                inline
-                                                label="B"
-                                                name="group1"
-                                                type="radio"
-                                                id="inline-radio-1"
-                                                value="B"
-                                                checked={section === 'B'}
-                                                onChange={handleRadioChangeSection}
-                                            />
-                                        </div>
-                                    </FormGroup>
-                                </Form>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Button variant="primary" onClick={displayPdf}>Primary</Button>
-                            </Col>
-                        </Row>
+            {forStudent &&
+                <>
+                    <Row className="mb-3">
+                        <Col>
+                            <Form>
+                                <FormGroup>
+                                    <div key="inline-radio" className="d-flex align-items-center">
+                                        <Form.Check
+                                            inline
+                                            label="L4-T1"
+                                            name="group1"
+                                            type="radio"
+                                            id="inline-radio-1"
+                                            value="L4-T1"
+                                            checked={lvlTerm === 'L4-T1'}
+                                            onChange={handleRadioChange}
+                                        />
+                                        <Form.Check
+                                            inline
+                                            label="L3-T2"
+                                            name="group1"
+                                            type="radio"
+                                            id="inline-radio-1"
+                                            value="L3-T2"
+                                            checked={lvlTerm === 'L3-T2'}
+                                            onChange={handleRadioChange}
+                                        />
+                                        <Form.Check
+                                            inline
+                                            label="L2-T2"
+                                            name="group1"
+                                            type="radio"
+                                            id="inline-radio-1"
+                                            value="L2-T2"
+                                            checked={lvlTerm === 'L2-T2'}
+                                            onChange={handleRadioChange}
+                                        />
+                                        <Form.Check
+                                            inline
+                                            label="L1-T2"
+                                            name="group1"
+                                            type="radio"
+                                            id="inline-radio-1"
+                                            value="L1-T2"
+                                            checked={lvlTerm === 'L1-T2'}
+                                            onChange={handleRadioChange}
+                                        />
+                                    </div>
+                                </FormGroup>
+                            </Form>
+                        </Col>
+                    </Row>
+                    <Row className="mb-3">
+                        <Col>
+                            <Form>
+                                <FormGroup>
+                                    <div key="inline-radio" className="d-flex align-items-center">
+                                        <Form.Check
+                                            inline
+                                            label="A"
+                                            name="group1"
+                                            type="radio"
+                                            id="inline-radio-1"
+                                            value="A"
+                                            checked={section === 'A'}
+                                            onChange={handleRadioChangeSection}
+                                        />
+                                        <Form.Check
+                                            inline
+                                            label="B"
+                                            name="group1"
+                                            type="radio"
+                                            id="inline-radio-1"
+                                            value="B"
+                                            checked={section === 'B'}
+                                            onChange={handleRadioChangeSection}
+                                        />
+                                    </div>
+                                </FormGroup>
+                            </Form>
+                        </Col>
+                    </Row>
+                </>
+            }
 
-                    </>
-                }
-                {forTeacher && <div>Content for Teacher</div>}
-                {forRoom && <div>Content for Room</div>}
 
+            {forTeacher &&
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>select initial</Form.Label>
+                            <Form.Control as="select" style={{ width: "25%" }} onChange={(e) => handleSelectInitial(e.target.value)}>
+                                {initials.map((item) => (
+                                    <option key={item.iinitial} value={item.initial}>
+                                        {item.initial}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+
+                    </Col>
+                </Row>
+            }
+            {forRoom &&
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>select room</Form.Label>
+                            <Form.Control as="select" style={{ width: "25%" }} onChange={(e) => handleSelectRoom(e.target.value)}>
+                                {rooms.map((item) => (
+                                    <option key={item.iinitial} value={item.room}>
+                                        {item.room}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </Col>
+                </Row>
+            }
+
+            <Row className="mb-3">
+                <Col>
+                    <Button variant="primary" onClick={displayPdf}>Show</Button>
+                </Col>
             </Row>
 
-            <Row>
-                {pdfData && (
-                    <object data={`data:application/pdf;base64,${pdfData}`} type="application/pdf" width="100%" height="600px">
-                        <p>It appears you don't have a PDF plugin for this browser. You can <a href={`data:application/pdf;base64,${pdfData}`}>click here to download the PDF file.</a></p>
-                    </object>
-                )}
-
-            </Row>
+            {pdfData && (
+                <Row>
+                    <Col>
+                        <div>
+                            <iframe
+                                title="PDF Viewer"
+                                src={URL.createObjectURL(pdfData)}
+                                width="100%"
+                                height="600px"
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            )}
         </Container>
 
     )
